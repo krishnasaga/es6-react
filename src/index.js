50cf06e9e4fcb691c9e05dc6504c00af438fef06
Mansur <mansurali.h@sonata-software.com>
2016-03-11 17:27:50 +0530

    static data replaced with REST endpoint by consuming fetch Pollyfill

--- src/index.js
+++ src/index.js
@@ -2,34 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import BooksList from './components/BooksList.js';
 import 'whatwg-fetch';
-const books = [
-  {
-    key: '1',
-    name: 'Clean Code',
-    author: 'Robert Martin',
-    description: 'Agile crafts man'
-  },
-  {
-    key: '2',
-    name: 'Pragmatic Programmers',
-    author: 'Andy Hunt, Dave Thomas',
-    description: 'Hello world'
-  },
-  {
-    key: '3',
-    name: 'Software Systems Architecture',
-    description: 'Hello world'
-  },
-  {
-    key: '4',
-    name: 'Patterns of Enterprise Application Architecture',
-    author: 'Martin Fowler',
-    description: 'Hello world'
-  }
-];
 
-fetch('./books.json').then(function(data){
-    console.log(data);
-  });
 
-ReactDOM.render(<BooksList list={books} />, document.getElementById('comps'));
+fetch('./books.json').then(function(response){
+  return response.json()
+}).then(function(json) {
+
+  ReactDOM.render(
+      <BooksList list={json} />,
+      document.getElementById('comps')
+  );
+
+}).catch(function(ex) {
+  console.log('parsing failed', ex)
+});
