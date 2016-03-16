46449a046aaf5c4fbc237df6f7f8aec4ae9c0ccd
Krishna Sagar Rayudu <krishnasagar.r@sonata-software.com>
2016-03-16 12:17:16 +0530

    Added test cases

--- test/unit/book.spec.js
+++ test/unit/book.spec.js
@@ -1,9 +1,69 @@
 var assert = require('assert');
-describe('Array', function() {
+const Code = require('code');
+const expect = Code.expect;
+import { booksNotLoaded, booksLoading, booksLoaded, booksLoadingFailed } from '../../src/actions/book.js';
+
+describe('Books http endpont', function() {
+    it('GET /books.json', function () {
+      expect(true).to.be.a.boolean().and.to.not.equal(false);
+    });
+});
+
+
+describe('loadBooks reducer', function() {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
+      expect(true).to.be.a.boolean().and.to.not.equal(false);
     });
   });
+});
+
+
+describe('Load Books actions', function() {
+  describe('Books loading....', function () {
+    it('status LOADING', function () {
+      const action = booksLoading();
+      expect(action.type).to.be.a.string().and.to.equal('BOOKS');
+      expect(action.status).to.be.a.string().and.to.equal('LOADING');
+    });
+  });
+
+  describe('Books books loaded!', function () {
+    it('status LOADED', function () {
+      const action = booksLoaded();
+      expect(action.type).to.be.a.string().and.to.equal('BOOKS');
+      expect(action.status).to.be.a.string().and.to.equal('LOADED');
+    });
+  });
+
+  describe('Books not loaded', function () {
+    it('status NOT_LOADED', function () {
+      const action = booksNotLoaded();
+      expect(action.type).to.be.a.string().and.to.equal('BOOKS');
+      expect(action.status).to.be.a.string().and.to.equal('NOT_LOADED');
+    });
+  });
+
+  describe('Books loadig faild', function () {
+    it('status LOADING_FAILED', function () {
+      const action = booksLoadingFailed();
+      expect(action.type).to.be.a.string().and.to.equal('BOOKS');
+      expect(action.status).to.be.a.string().and.to.equal('LOADING_FAILED');
+    });
+  });
+});
+
+describe('reduce booksApp', function() {
+    it('computes state for loading....', function () {
+      expect(true).to.be.a.boolean().and.to.not.equal(false);
+    });
+    it('computes state for loaded!', function () {
+      expect(true).to.be.a.boolean().and.to.not.equal(false);
+    });
+    it('computes state for not loaded', function () {
+      expect(true).to.be.a.boolean().and.to.not.equal(false);
+    });
+    it('computes state for loading faild! ', function () {
+      expect(true).to.be.a.boolean().and.to.not.equal(false);
+    });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,11 @@ module.exports = {
      test: /\.js?$/,
      loader: 'babel-loader',
      exclude: /node_modules/
-   }
+   },
+    {
+      test: /\.scss$/,
+      loaders: 'style!css!sass'
+    }
   ]
   }
 };
